42c06f35d07211b5397201387967d27efd83b609
Adeyemi <adeyemisam125@gmail.com>
2022-06-29 17:20:03 -0800

    Updated Urls.py

--- goldsite/urls.py
+++ goldsite/urls.py
@@ -19,10 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
     path('users/', include('users.urls')),
-    path('', admin.site.urls),
+ 
  
 ]
 
