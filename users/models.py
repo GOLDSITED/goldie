75b7c10257b64a6c9d3bfcde5f080f0021830eb1
GOLDSITED <108181254+GOLDSITED@users.noreply.github.com>
2022-10-06 18:44:32 -0700

    Update models.py

--- users/models.py
+++ users/models.py
@@ -9,3 +9,15 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile.jpg',upload_to='profile_pictures')
     contact_number = models.CharField(max_length=100,default='09051485282')
+    
+    
+    
+class Contact(models.Model):
+    fullname = models.CharField(max_length=20, null=True)
+    phonenumber = models.CharField(max_length=11, null=True)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
